625f81a0792641d208d98f93c086a71608c5b2f3
Derek Belloni <derekbelloni10@gmail.com>
2025-04-03 08:10:56 -0600

    d3geo rendering to the screen

--- client/index.js
+++ client/index.js
@@ -3,18 +3,21 @@ import { renderMap } from "./services/IdahoMap";
 import D3Config from "./services/d3";
 
 
-let d3Dir = null;
+// let d3Dir = null;
 
-document.addEventListener('DOMContentLoaded', async () => {
-    const d3Config = new D3Config();
-    d3Dir = await d3Config.getD3Directory();
-})
+// document.addEventListener('DOMContentLoaded', async () => {
+//     const d3Config = new D3Config();
+//     d3Dir = await d3Config.getD3Directory();
+// })
 
-// json('idaho_counties.geojson')
-//     .then((data) => {
-//         const counties = data.features;
-//         renderMap(counties);
-// }).catch((error) => {
-//         console.error("Couldn't extract counties from geo json: ", error);
-// });
+document.addEventListener('DOMContentLoaded', () => {
+    json('idaho_counties.geojson')
+        .then((data) => {
+            const counties = data.features;
+            console.log(counties)
+            renderMap(data.features);
+    }).catch((error) => {
+            console.error("Couldn't extract counties from geo json: ", error);
+    });
+});
 
--- server/index.js
+++ server/index.js
@@ -7,14 +7,14 @@ const app = express();
 const port = 4000;
 
 const __dirname = import.meta.dirname;
-const d3Dir = path.join(__dirname, '..', 'node_modules', 'd3', 'dist');
+// const d3Dir = path.join(__dirname, '..', 'node_modules', 'd3', 'dist');
 
-const d3FilePath = path.join(d3Dir, 'd3.js');
-if (fs.existsSync(d3FilePath)) {
-  console.log('d3.js file exists at:', d3FilePath);
-} else {
-  console.log('d3.js file NOT found at:', d3FilePath);
-}
+// const d3FilePath = path.join(d3Dir, 'd3.js');
+// if (fs.existsSync(d3FilePath)) {
+//   console.log('d3.js file exists at:', d3FilePath);
+// } else {
+//   console.log('d3.js file NOT found at:', d3FilePath);
+// }
 
 app.use(cors({
     origin: 'http://localhost:5173', 
@@ -29,7 +29,6 @@ app.use(cors({
 // app.use('/d3', express.static(d3Dir));
 
 app.get('/api/', (req, res) => {
-    console.log('req: ', req)
     res.send('what up');
 });
 
